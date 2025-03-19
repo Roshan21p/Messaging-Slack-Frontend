5d8e3675a3f474db7948bf1fd3817707423a2ddb
shendi321 <roshanprajapati2004@gmail.com>
2025-03-19 23:49:09 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-center text-xl pt-10 bg-gradient-to-br from-blue-500 to-indigo-700'>Hello</div>
+    </>
+  )
+}
+
+export default App
