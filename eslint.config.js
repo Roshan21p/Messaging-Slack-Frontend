6d0a8fe466d814098b9fe288765956a9772f3ef6
shendi321 <roshanprajapati2004@gmail.com>
2025-03-20 00:40:40 +0530

    Added eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import react from 'eslint-plugin-react';
 
 export default [
   { ignores: ['dist'] },
@@ -17,6 +19,8 @@ export default [
       },
     },
     plugins: {
+      react,
+      'simple-import-sort': simpleImportSort,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -28,6 +32,9 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'react/prop-types': 'off'
     },
   },
 ]
