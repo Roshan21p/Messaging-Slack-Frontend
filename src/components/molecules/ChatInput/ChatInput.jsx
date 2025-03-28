9a4a3549a2f126e27d1a1f73070d3edf634d5c1a
shendi321 <roshanprajapati2004@gmail.com>
2025-03-28 06:53:50 +0530

    Added message renderer

--- src/components/molecules/ChatInput/ChatInput.jsx
+++ src/components/molecules/ChatInput/ChatInput.jsx
@@ -1,8 +1,12 @@
 import { Editor } from '@/components/atoms/Editor/Editor';
+import { MessageRenderer } from '@/components/atoms/MessageRenderer/MessageRenderer';
+import { useState } from 'react';
 
 export const ChatInput = () => {
+   const [text, setText] = useState('');
    async function handleSubmit({ body }) {
       console.log(body);
+      setText(body);
    }
    return (
       <div className="px-5 w-full">
@@ -13,6 +17,7 @@ export const ChatInput = () => {
             disabled={false}
             defaultValue=""
          />
+         {text && <MessageRenderer value={text} />}
       </div>
    );
 };
